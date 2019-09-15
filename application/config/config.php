d19abb6274b1d6c7ee5c8c3d70af71fc74616e34
taokeli <55184348+taokeli@users.noreply.github.com>
2019-09-15 11:05:04 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= 'https://github.com/taokeli/github.io';
 
 /*
 |--------------------------------------------------------------------------
